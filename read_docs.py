806401156ef1ebc02b03b5235b0d78e8222ee0ed
Андрей <andrey@MacBook-Pro-Andrej.local>
2026-03-09 02:34:40 +0300

    chore: remove test scripts

--- read_docs.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://yandex.cloud/ru/docs/search-api/operations/search"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
-for pre in soup.find_all("pre"):
-    print(pre.text)
